eac7d313c91586e31f43f0ea313d0576891141e7
NickPels <54907549+NickPels@users.noreply.github.com>
2021-07-24 18:26:18 -0400

    Add files via upload

---
+++ navigation.php
@@ -0,0 +1,42 @@
+<?php
+    $Page = "Navigation";
+    $Path = "../";
+    include $path.'nav.php'
+?>
+    
+    <body>
+        <h1>Navigation</h1>
+        <div><h3>Heirarchy</h3>
+            <p>Sibling directories exist in the same directory
+                Parent directories have other directories inside of them
+                Child directories are inside of a parent directory</p>
+        </div>
+        <div>
+            <h3>Absolute Path</h3>
+            <p>Starts with “/”
+                Unless the desired location is in the current working directory, you must start with “/home”, and work down.
+                “/” separates every directory/file</p>
+        </div>
+        <div>
+            <h3>Relative Path</h3>
+            <p>Parent to child: cd DirectoryName
+                Child to parent: cd ..
+                Sibling to sibling: cd ../DirectoryName
+                To home directory: ~ or cd
+                Use pwd (print working directory) to show where you are</p>
+        </div>
+        <div>
+            <h3>Commands</h3>
+            <ul>
+                <li>cd: change directory</li>
+                <li>mkdir pathname: creates a new, empty directory at the given point</li>
+                <li>rm pathname: removes non-empty directory</li>
+                <li>rmdir pathname: removes empty directory</li>
+                <li>ls pathname: lists contents of the given directory</li>
+            </ul>
+        </div>
+
+
+
+
+    </body>
---
+++ permissions.php
@@ -0,0 +1,42 @@
+<?php
+    $Page = "Permissions";
+    $Path = "../";
+    include $path.'nav.php'
+?>
+    
+    <body>
+        <h1>Permissions</h1>
+        <ul>
+        <p>ls-l shows a series of 10 characters next to files and directories</p>
+            <p>The first character signifies the type</p>
+            <li>A dash(-) is a regular file, d is a directory, and l is a symbolic link</li>
+            
+            <p>The rest give ownership permission</p>
+            <li>Permissions are r (read), w (write), and x(execute)</li>
+            <li>r, w and x all have numerical values</li>
+            <ul>
+                <li>r=4</li>
+                <li>w=2</li>
+                <li>x=1</li>
+            </ul>
+            <p>numerical permissions can be given</p>
+            <li>775 is common</li>
+            <ul>
+                <li>The first 7 (4+2+1) gives all permissions to the user</li>
+                <li>The second 7 gives all permissions to the group</li>
+                <li>The 5 (4+1) gives read and execute permissions to others</li>
+            </ul>
+            <p>Symbolic permissions can be given to the user (u), group (g), or others (o)</p>
+            <p>Operations can be used to alter permissions</p>
+            <ul>
+                <li>+: add symbolic-combo to file/directory</li>
+                <li>-: subtract symbolic-combo from file/directory</li>
+                <li>=: set file/directory to symbolic-combo</li>
+            </ul>
+        </ul>
+        <h3>Examples</h3>
+        <ul>
+            <li>Chmod uo+w dir		Adds write permissions to user and others</li>
+            <li>Chmod a=rwx		Sets all permissions to rwx</li>
+        </ul>
+    </body>
