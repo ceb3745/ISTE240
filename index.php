558714cd09f20dd57cc23c4a2a6d437328da0ca4
Carson Bloomingdale <ceb3745@g.rit.edu>
2021-08-03 16:35:04 -0400

    incorperate Rewans styles

--- index.php
+++ index.php
@@ -1,35 +1,42 @@
-
-        <?php 
-            $Page = "Home";
-            $Path = "";
-            $Style = $Path."/assets/css/main.css";
-        include($Path.'assets/inc/nav.php'); ?>
-
-    
-    <main>
-        <h1>Welcome to Unix!</h1>
-        <p>
-            Unix is a computer Operating System which is capable of handling activities from multiple users at the same time. In this UNIX tutorial for beginners series, we will be practicing it as it is freely available. The training will require you to execute certain commands. Make sure to practice them!</p>
-            
-        <h2>Prerequisites for learning UNIX Tutorial?</h2>
-            <p>Nothing. This tutorial has been prepared for beginners to help them understand the basics to advanced concepts covering Unix commands and various utilities.</p>
-    
-        <p>By the end of this series, you will understand the basic and advanced concepts of Unix Architecture, Unix Commands, and the applications such as File Management, etc.</p>
-
-       <h2>Courses for you to learn:</h2>
-        <div id="categorylinks">
-            <a class="categorylink" href="pages/category1.php">
-                <p>Category 1</p>
+<?php 
+   $Page = "Home";
+   $Path = "";
+   $Style = $Path."/assets/css/main.css";
+   include($Path.'assets/inc/nav.php'); ?>
+<main>
+   <h1>Welcome to Unix!</h1>
+   <p>
+      Unix is a computer Operating System which is capable of handling activities from multiple users at the same time. In this UNIX tutorial for beginners series, we will be practicing it as it is freely available. The training will require you to execute certain commands. Make sure to practice them!
+   </p>
+   <h2>Prerequisites for learning UNIX Tutorial?</h2>
+   <p>Nothing. This tutorial has been prepared for beginners to help them understand the basics to advanced concepts covering Unix commands and various utilities.</p>
+   <p>By the end of this series, you will understand the basic and advanced concepts of Unix Architecture, Unix Commands, and the applications such as File Management, etc.</p>
+   <h2>Courses for you to learn:</h2>
+   <div class="card-main">
+      <div class="card bg-card">
+         <div class="card-content">
+            <a href="pages/category1.php">
+               <p class="card-category  font-sfprotsb f-17">Introduction to Unix<i class="fas fa-chevron-right fa-sm"></i></p>
             </a>
-            <a class="categorylink" href="pages/category2.php">
-                <p>Category 2</p>
+         </div>
+      </div>
+      <div class="card bg-card">
+         <div class="card-content">
+            <a href="pages/category2.php">
+               <p class="card-category  font-sfprotsb f-17">Basics of Unix<i class="fas fa-chevron-right fa-sm"></i></p>
             </a>
-            <a class="categorylink" href="pages/category3.php">
-                <p>Category 3</p>
+         </div>
+      </div>
+      <div class="card bg-card">
+         <div class="card-content">
+            <a href="pages/category3.php">
+               <p class="card-category  font-sfprotsb f-17">Advanced Unix<i class="fas fa-chevron-right fa-sm"></i></p>
             </a>
-        </div>
-    </main>
+         </div>
+      </div>
+   </div>
+</main>
 <?php
-    $Path = "";
-    $Right = "pages/category1.php";
-include($Path.'assets/inc/footer.php'); ?>
+   $Path = "";
+   $Right = "pages/category1.php";
+   include($Path.'assets/inc/footer.php'); ?>
--- pages/category1.php
+++ pages/category1.php
@@ -16,28 +16,44 @@ include($Path.'assets/inc/nav.php'); ?>
     </ul>
 
     <main>
-        <h1>Category 1 -- Introduction to Unix</h1>
+        <h1 class="font-sfprodsb f-34">Category 1 -- Introduction to Unix</h1>
         <p>
             Welcome to the Category 1!
         <br>
             In this tutorial, you will get basic concepts of Unix, how to open this application, and why Unix is essential to learn.
 
         </p>
-        <h2>
+        <h2  class="font-sfprodsb f-22">
             Lessons 
         </h2>
 
-            <ul id="lessons">
-        <li>
-            <a href="./definition.php">What is UNIX</a>
-        </li>
-        <li>
-            <a href="./howto.php">How to Use</a>
-        </li>
-        <li>
-            <a href="./important.php">Why is UNIX important</a>
-        </li>
-    </ul>
+        <div class="card-main">
+          <div class="card">
+  
+            <div class="card-content">
+              <a href="./definition.php">
+                <p class="card-category  font-sfprotsb f-17">What is UNIX<i class="fas fa-chevron-right fa-sm"></i></p>
+              </a>
+            </div>
+          </div>
+  
+          <div class="card">
+  
+            <div class="card-content">
+              <a href="./howto.php">
+                <p class="card-category  font-sfprotsb f-17">How to Use<i class="fas fa-chevron-right fa-sm"></i></p>
+              </a>
+            </div>
+          </div>
+          <div class="card">
+  
+            <div class="card-content">
+              <a href="./important.php">
+                <p class="card-category  font-sfprotsb f-17">Why is UNIX important<i class="fas fa-chevron-right fa-sm"></i></p>
+              </a>
+            </div>
+        </div>
+    </div>
             
    
     </main>
