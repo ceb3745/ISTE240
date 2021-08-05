d7708496e20e54fd1052c0211b230c95ca8c5a88
Carson Bloomingdale <ceb3745@g.rit.edu>
2021-08-04 23:18:54 -0400

    thic

---
+++ pages/resources.php
@@ -0,0 +1,32 @@
+  <?php
+    $Page = "Basic Commands";
+    $Path = "../";
+    $Style = $Path."/assets/css/dynamicstyle.css";
+include($Path.'assets/inc/nav.php'); ?>
+
+
+        <main>
+        <h1>Additional Resources</h1>
+        <div>
+            <p>Check out these additional resources for more information on Unix!
+			</p>
+            
+            <ul>
+                
+                <li><a href="http://www.ee.surrey.ac.uk/Teaching/Unix/unixintro.html">EE Surrey Unix Tutorial</a></li>
+                <li><a href="https://www.techopedia.com/definition/4637/unix">Technopedia</a></li>
+                <li><a href="https://www.dummies.com/computers/macs/mac-operating-systems/how-to-use-terminal-to-display-unix-on-your-mac/">Unix on a Mac</a></li>
+                <li><a href="https://www.oreilly.com/library/view/learning-unix-for/0596004702/ch01.html">More Unix on Mac</a></li>
+                <li><a href="https://linuxize.com/post/linux-cd-command/">CD on Linux</a></li>
+				<li><a href="https://www.geeksforgeeks.org/absolute-relative-pathnames-unix/">Absolute and relative pathnames in Unix</a></li>
+                
+            </ul>
+           
+        </div>
+            </main>
+
+<?php
+    $Path = "../";
+    $Left = "./category2.php";
+    $Right = "./commands.php";
+include($Path.'assets/inc/footer.php'); ?>
